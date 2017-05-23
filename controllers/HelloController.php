374f073b59bfb0a019d32c67303b03c435dc5435
ks-satra <Satra.e@yru.ac.th>
2017-05-23 00:46:19 -0700

    first-yii2

--- controllers/HelloController.php
+++ controllers/HelloController.php
@@ -4,7 +4,15 @@ class HelloController extends \yii\web\Controller{
 
 	public function actionIndex(){
 		//echo "น้องที่รัก";
-		return $this->render('index');
+		//return $this->render('index');
+		$weight = 60;
+
+		return $this-> render('index',[
+			'firstname' => 'Satra',
+			'lastname' => 'Eadtrong',
+			'weigth' => $weight
+		]
+	);
 	}
 
 	public function actionProfile(){
--- views/hello/index.php
+++ views/hello/index.php
@@ -1,1 +1,14 @@
-<h2>YRU love4444 </h2>
+<center>
+<h2>Welcome to Website</h2>
+
+  <label>ชื่อ - สกุล </labal>
+    <?=$firstname?> <?=$lastname?><br/><br/>
+
+    <label>น้ำหนัก </label> <?=$weigth?><br/>
+
+      <ul id="w1" class="navbar-nav navbar-right nav">
+        <li><a href="https://www.facebook.com/SL.Satra.ER">Facebook ของฉัน</a></li><br/>
+        <li><a href="https://www.youtube.com/watch?v=NsN3CMUb-VY">เพลงที่ชอบ </a></li>
+      </ul>
+
+</center>
